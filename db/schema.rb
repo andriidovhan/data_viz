e6ae87022d7b2fa0b173907d3ae5161cab7eba5e
Andrii Dovhan <andrii.dovhan@onapp.com>
2017-10-06 15:42:34 +0300

    rails init and rake task for moving csv data to table

---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171006120832) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "session_id"
+    t.string  "started_by"
+    t.string  "created_at"
+    t.string  "summary_status"
+    t.float   "duration"
+    t.float   "worker_time"
+    t.integer "bundle_time"
+    t.integer "num_workers"
+    t.string  "branch"
+    t.string  "commit_id"
+    t.integer "started_tests_count"
+    t.integer "passed_tests_count"
+    t.integer "failed_tests_count"
+    t.integer "pending_tests_count"
+    t.integer "skipped_tests_count"
+    t.integer "error_tests_count"
+  end
+
+end
---
+++ lib/tasks/csv_import.rake
@@ -0,0 +1,31 @@
+require 'csv'
+
+namespace :csv_import do
+  desc "move csv data from file"
+  task upload_data: :environment do
+    csv_text = File.read("/Users/andrii/Documents/cyber_craft/session_history.csv")
+    csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+    csv.each do |row|
+      t = Session.new
+      t.session_id = row['session_id']
+      t.started_by = row['started_by']
+      t.created_at = row['created_at']
+      t.summary_status = row['summary_status']
+      t.duration = row['duration'].to_f
+      t.worker_time = row['worker_time']
+      t.bundle_time = row['bundle_time']
+      t.num_workers = row['num_workers']
+      t.branch = row['branch']
+      t.commit_id = row['commit_id']
+      t.started_tests_count = row['started_tests_count']
+      t.passed_tests_count = row['passed_tests_count']
+      t.failed_tests_count = row['failed_tests_count']
+      t.pending_tests_count = row['pending_tests_count']
+      t.skipped_tests_count = row['skipped_tests_count']
+      t.error_tests_count = row['error_tests_count']
+      t.save
+      puts "#{t.session_id} saved"
+    end
+  end
+
+end
