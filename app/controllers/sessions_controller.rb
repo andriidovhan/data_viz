d5d7445add28c913b6eabd88d056b255a37efb26
Andrii Dovhan <andrii.dovhan@onapp.com>
2017-10-06 22:19:07 +0300

    add abnormal chart, add scope to the Session model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,22 @@
 class SessionsController < ApplicationController
   def index
-    # @bundle_time = Session.all
-    @sessions ||= Session.all
+    @sessions = Session.all
+    @abnormal = calc_abnormal
   end
+end
+
+private
+
+def calc_abnormal
+  total = @sessions.group_and_count_per_day
+  failed = @sessions.failed.group_and_count_per_day
+  abnormal = {}
+  total.each do |k,v|
+    unless total[k] == 0
+      temp=((failed[k].to_f/total[k])*100).round(2)
+      abnormal[k]=temp
+    end
+  end
+
+  abnormal
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,4 @@
 class Session < ActiveRecord::Base
+  scope :failed, -> { where.not(summary_status: 'passed') }
+  scope :group_and_count_per_day, -> { group_by_day(:created_at, format: "%B %d, %Y").count }
 end
