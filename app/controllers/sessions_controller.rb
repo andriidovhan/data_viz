0c76663c49d1afa930d34d01d4495187c2f4aadd
Andrii Dovhan <andrii.dovhan@onapp.com>
2017-10-09 16:15:16 +0300

    move logic to model; use class method instead of scope

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,5 @@
 class SessionsController < ApplicationController
   def index
     @sessions = Session.all
-    @abnormal = calc_abnormal
   end
-end
-
-private
-
-def calc_abnormal
-  total = @sessions.group_and_count_per_day
-  failed = @sessions.failed.group_and_count_per_day
-  abnormal = {}
-  total.each do |k,v|
-    unless total[k] == 0
-      temp=((failed[k].to_f/total[k])*100).round(2)
-      abnormal[k]=temp
-    end
-  end
-
-  abnormal
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,23 @@
 class Session < ActiveRecord::Base
   scope :failed, -> { where.not(summary_status: 'passed') }
-  scope :group_and_count_per_day, -> { group_by_day(:created_at, format: "%B %d, %Y").count }
+
+  class << self
+    def group_and_count_per_day
+      group_by_day(:created_at, format: "%B %d, %Y").count
+    end
+
+    def calc_abnormal
+      total = group_and_count_per_day
+      fail = failed.group_and_count_per_day
+      abnormal = {}
+      total.each do |k,v|
+        unless total[k] == 0
+          temp=((fail[k].to_f/total[k])*100).round(2)
+          abnormal[k]=temp
+        end
+      end
+
+      abnormal
+    end
+  end
 end
